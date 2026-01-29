59fbdd6b9d7a970d307435adf1b34f0327398f56
Ruchismita12 <138961626+Ruchismita12@users.noreply.github.com>
2026-01-29 21:11:38 +0530

    Time: 50 ms (50.77%), Space: 29.2 MB (45.28%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,3 +1,57 @@
+/*class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n=nums.size();
+        vector<vector<int>> ans;
+        set<vector<int>> s;
+        sort(nums.begin(),nums.end());
+        //2 pointer
+        
+        for(int i=0;i<n;i++)
+        {
+            int j=i+1;
+            int k=n-1;
+            while(j<k)
+            {
+                int sum=nums[i]+nums[j]+nums[k];
+                if(sum==0)
+                {
+                    vector<int> temp;
+                    temp.push_back(nums[i]);
+                    temp.push_back(nums[j]);
+                    temp.push_back(nums[k]);
+                    sort(temp.begin(),temp.end());
+                    if(s.find(temp)==s.end())
+                    {
+                        ans.push_back(temp);
+                        s.insert(temp);
+                    }
+                    j++;
+                    k--;
+                }
+                else if(sum > 0)
+                {
+                    k--;
+
+                }
+                else
+                {
+                    j++;
+                }
+            }
+
+        }
+        return ans;
+        
+    }
+};
+*/
+
+
+
+
+/*
+
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
@@ -31,5 +85,51 @@ public:
     }
     return ans;
         
+    }
+};
+
+*/
+
+
+
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> res;
+        int n = nums.size();
+
+        for (int i = 0; i < n - 2; i++) {
+            // Skip duplicate fixed elements
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+
+            int j = i + 1, k = n - 1;
+
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+
+                if (sum == 0) {
+                    res.push_back({nums[i], nums[j], nums[k]});
+                    j++;
+                    k--;
+
+                    // Skip duplicate second elements
+                    while (j < k && nums[j] == nums[j - 1])
+                        j++;
+
+                } else if (sum < 0) {
+                    j++;
+                } else {
+                    k--;
+                }
+            }
+        }
+
+        return res;
     }
 };
