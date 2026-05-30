190f7e01134df14b3eef5581233e70e2478cd794
Ruchismita12 <138961626+Ruchismita12@users.noreply.github.com>
2026-05-30 15:15:46 +0530

    Time: 2 ms (39.48%), Space: 14.4 MB (11.93%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,59 +1,36 @@
+
 class Solution {
 public:
-    void solve(int index,vector<int> ds,set<vector<int>> &res,vector<int>& nums)
+void subsetsWithDupUtil(vector<int>& nums,int i,vector<int>& output,set<vector<int>>& ans )
+{
+    //B.c
+    if(i>=nums.size())
     {
-        if(index==nums.size())
-        {
-            sort(ds.begin(),ds.end());
-            res.insert(ds);
-            return;
-        }
-        ds.push_back(nums[index]);
-        solve(index+1,ds,res,nums);
-        ds.pop_back();
-        solve(index+1,ds,res,nums);
-
-    }
-    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        vector<vector<int>> ans; //storing the ans
-        set<vector<int>> res; //for storing unique subsets
-        vector<int> ds;
-        solve(0,ds,res,nums);
-        for (auto it = res.begin(); it != res.end(); it++) {
-           ans.push_back( * it);
-        }
         
-        return ans;
+        ans.insert(output);
+        return;
+        
+        
+    }
+
+    //exclude
+    subsetsWithDupUtil(nums,i+1,output,ans);
+
+    //include
+    output.push_back(nums[i]);
+    subsetsWithDupUtil(nums,i+1,output,ans);
+    output.pop_back();
+
+
+}
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        vector<int> output;
+        set<vector<int>> ans;
+        
+        subsetsWithDupUtil(nums,0,output,ans);
+
+        return vector<vector<int>>(ans.begin(),ans.end());
         
     }
 };
-
-
-
-
-
-/*
-class Solution {
-  public:
-    void fun(vector < int > & nums, int index, vector < int > ds, set < vector < int >> & res) {
-      if (index == nums.size()) {
-        sort(ds.begin(), ds.end());
-        res.insert(ds);
-        return;
-      }
-      ds.push_back(nums[index]);
-      fun(nums, index + 1, ds, res);
-      ds.pop_back();
-      fun(nums, index + 1, ds, res);
-    }
-  vector < vector < int >> subsetsWithDup(vector < int > & nums) {
-    vector < vector < int >> ans;
-    set < vector < int >> res;
-    vector < int > ds;
-    fun(nums, 0, ds, res);
-    for (auto it = res.begin(); it != res.end(); it++) {
-      ans.push_back( * it);
-    }
-    return ans;
-  }
-};  */
