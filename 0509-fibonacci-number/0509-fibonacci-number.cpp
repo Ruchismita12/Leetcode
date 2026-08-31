e082433c96d6518f00b6a8fdaf42496c624676b1
Ruchismita12 <138961626+Ruchismita12@users.noreply.github.com>
2026-08-31 23:49:32 +0530

    Time: 14 ms (22.32%), Space: 7.9 MB (38.49%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,14 +1,10 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n==0)
-        {
-            return 0;
-        }
-        if(n==1)
-        {
-            return 1;
-        }
+        //B.C
+        if(n==0) return 0;
+        if(n==1) return 1;
+        //Recursion
         int ans=fib(n-1)+fib(n-2);
         return ans;
         
